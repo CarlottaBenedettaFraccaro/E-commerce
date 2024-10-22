0580459ba4b2ce5a4e18c5d75c63e0ee62f4b66a
Fran Bejarano <Franbejaranom@gmail.com>
2024-10-22 22:23:47 +0200

    desarrollo de middlewares

--- index.js
+++ index.js
@@ -1,25 +1,7 @@
-import express from "express";
+import server from "./src/server.js";
 
-import {
-  homeController,
-  useriosController,
-  productosCotroller,
-} from "./src/controllers/controladores.js";
+const PORT = process.env.PORT || 3001;
 
-const server = express();
-
-server.use((req, res, next) => {
-  console.log("Solicitud recibida " + new Date());
-
-  next();
+server.listen(PORT, () => {
+  console.log(`Servidor escuchando en puerto ${PORT}`);
 });
-
-server.get("/", homeController);
-server.get("/usuarios", useriosController);
-server.get("/productos", productosCotroller);
-
-//server.post --> crea un nuevo recurso
-//server.put --> modifica un recurso ya creado
-//server.delete --> elimina un recurso
-
-server.listen(3001, () => console.log("Servidor escuchando en puerto 3001"));
