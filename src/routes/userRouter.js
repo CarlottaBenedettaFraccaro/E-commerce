05b0462dd14c80676c0889bd511cf137575f2ef1
Alex Riera <alrron14@hotmail.com>
2024-10-28 19:11:55 +0100

    Update userRouter.js

--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -1,9 +1,11 @@
 import express from "express";
 
-import editUserPasswordController from "../controllers/EditUserPasswordController";
+import editUserPasswordController from "../controllers/editUserPasswordController";
+import cartAddProductController from "../controllers/cartAddProductController";
 
 const userRouter = express.Router();
 
 userRouter.post("/users/password", editUserPasswordController);
+userRouter.post('/cart', cartAddProductController);
 
 export default userRouter;
