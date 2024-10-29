fb4b3b1ff706adff06629e3f8d359e8c29216d02
Rodrigo Usín <rousind2@gmail.com>
2024-10-29 18:38:24 +0100

    completar UserRouter.js

--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -1,9 +1,18 @@
 import express from "express";
 
 import editUserPasswordController from "../controllers/EditUserPasswordController";
+import registerUserController from "../controllers/users/registerUserController";
+import validaterUserController from "../controllers/users/validaterUserController";
+import sendRecoverPassController from "../controllers/users/sendRecoverPassController";
+import loginUserController from "../controllers/users/loginUserController";
 
 const userRouter = express.Router();
 
 userRouter.post("/users/password", editUserPasswordController);
+userRouter.post("/users/register", registerUserController);
+userRouter.post("/users/validater", validaterUserController);
+userRouter.post("/users/recover_password", sendRecoverPassController);
+userRouter.post("/users/login", loginUserController);
+//userRouter.post("/users/password", editUserPasswordController);
 
 export default userRouter;
