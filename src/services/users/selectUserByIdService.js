8d97e637aff40e99a63c96c24eaa60e8013ca5eb
Alex Riera <alrron14@hotmail.com>
2024-10-29 19:23:10 +0100

    Update selectUserByIdService.js

--- src/services/users/selectUserByIdService.js
+++ src/services/users/selectUserByIdService.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { generate } from "randomstring";
 import getPool from "../../database/getPool.js";
 import generateErrorsUtils from "../../utils/generateErrorsUtils.js";
@@ -8,7 +7,7 @@ const selectUserByIdService = async (userId) => {
 
   const [user] = await pool.query(
     `
-        select id, email, firstName, lastName, avatar
+        select id, first_name, surname, email
         from users
         where id=?
         `,
@@ -23,29 +22,3 @@ const selectUserByIdService = async (userId) => {
 };
 
 export default selectUserByIdService;
-=======
-import { generate } from "randomstring";
-import getPool from "../../database/getPool.js";
-import generateErrorsUtils from "../../utils/generateErrorsUtils.js";
-
-const selectUserByIdService = async (userId) => {
-  const pool = await getPool();
-
-  const [user] = await pool.query(
-    `
-        select id, email, firstName, lastName, avatar
-        from users
-        where id=?
-        `,
-    [userId]
-  );
-
-  if (!user.length) {
-    throw generateErrorsUtils("el usuario no existe", 404);
-  }
-
-  return user[0];
-};
-
-export default selectUserByIdService;
->>>>>>> b70d090309b7731c80490c532908229153c368de
