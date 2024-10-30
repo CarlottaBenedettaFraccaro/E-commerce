2efb075ba43f83f78ac1e5266ee95cd8b899e249
MariCarmenMolina <mcmosa1991@gmail.com>
2024-10-30 19:53:55 +0100

    Update userRouter.js

--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -1,23 +1,24 @@
 import express from "express";
+import registerUserController from "../controllers/users/registerUserController.js";
+import loginUserController from "../controllers/users/loginUserController.js";
+import validaterUserController from "../controllers/users/validaterUserController.js";
 import authUser from "../middlewares/authUser.js";
 import adminUser from "../middlewares/adminUser.js";
-import editUserPasswordController from "../controllers/editUserPasswordController.js";
-import registerUserController from "../controllers/users/registerUserController.js";
-import validaterUserController from "../controllers/users/validaterUserController.js";
 import sendRecoverPassController from "../controllers/users/sendRecoverPassController.js";
-import loginUserController from "../controllers/users/loginUserController.js";
 import newProductController from "../controllers/newProductController.js";
+import editUserPasswordController from "../controllers/editUserPasswordController.js";
 import listCategoriesController from '../controllers/listCategoriesController.js';
 
 const userRouter = express.Router();
 
-userRouter.post("/users/password", editUserPasswordController);
 userRouter.post("/users/register", registerUserController);
-userRouter.post("/users/validater", validaterUserController);
-userRouter.post("/users/recover_password", sendRecoverPassController);
+userRouter.get('/users/validate/:registrationCode', validaterUserController);
 userRouter.post("/users/login", loginUserController);
-//userRouter.post("/users/password", editUserPasswordController);
-userRouter.post("/newproduct", authUser, adminUser, newProductController);
+
+userRouter.put("/users/password", editUserPasswordController);
+userRouter.post('/users/password/recover', sendRecoverPassController);
+
+userRouter.get("/newproduct", authUser, adminUser, newProductController);
 router.get('/categories', listCategoriesController);
 
 export default userRouter;
