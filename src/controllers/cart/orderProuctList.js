2c987e90f5da568f3ff71099045da76328038d42
Rodrigo Usín <rousind2@gmail.com>
2024-10-30 19:03:01 +0100

    Enpoint y server de la lista del carrito ok

--- src/controllers/cart/orderProuctList.js
+++ src/controllers/cart/orderProuctList.js
@@ -1,40 +1,12 @@
-import orderController from "../controllers/orderController.js";
-import selectUserByIdService from "../../services/users/selectUserByIdService.js"; // Corregido "sevices" a "services"
 import generateErrorsUtils from "../../utils/generateErrorsUtils.js";
-
-const user = await selectUserByIdService(userId);
-if (!user) {
-  throw generateErrorsUtils("Usuario no encontrado", 404);
-}
+import orderProductListCartService from "../../services/cart/orderProductListCartService.js";
 
 const orderViewFinalController = async (req, res, next) => {
   try {
     const { userId } = req.user; // Asegúrate de que req.user sea donde obtienes el userId
 
     // Obtener el carrito del usuario
-    const cart = await orderController.getOrderByUserId(userId);
-
-    if (!cart || cart.items.length === 0) {
-      throw generateErrorsUtils(
-        "Carrito vacío o no encontrado",
-        404 // Código de estado para recurso no encontrado
-      );
-    }
-
-    // Calcular subtotal y total del carrito
-    let total = 0;
-    const items = cart.items.map((item) => {
-      const subtotal = item.quantity * item.product.price;
-      total += subtotal;
-
-      return {
-        productId: item.product._id,
-        name: item.product.name,
-        price: item.product.price,
-        quantity: item.quantity,
-        subtotal,
-      };
-    });
+    const cart = await orderProductListCartService(userId);
 
     // Enviar respuesta con los detalles del carrito
     res.status(200).json({
@@ -46,7 +18,7 @@ const orderViewFinalController = async (req, res, next) => {
       },
     });
   } catch (error) {
-    next(error); // Pasar el error al middleware de manejo de errores
+    next(generateErrorsUtils); // Pasar el error al middleware de manejo de errores
   }
 };
 
