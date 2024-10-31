6089ad7b547263effe41b5f3ac743d339a2db604
Rodrigo Usín <rousind2@gmail.com>
2024-10-31 09:40:05 +0100

    cambio en userRouter

--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -5,20 +5,20 @@ import validaterUserController from "../controllers/users/validaterUserControlle
 import authUser from "../middlewares/authUser.js";
 import adminUser from "../middlewares/adminUser.js";
 import sendRecoverPassController from "../controllers/users/sendRecoverPassController.js";
-import newProductController from "../controllers/newProductController.js";
-import editUserPasswordController from "../controllers/editUserPasswordController.js";
-import listCategoriesController from '../controllers/listCategoriesController.js';
+import newProductController from "../controllers/cart/newProductController.js";
+import editUserPasswordController from "../controllers/users/editUserPasswordController.js";
+import listCategoriesController from "../controllers/cart/listCategoriesController.js";
 
 const userRouter = express.Router();
 
 userRouter.post("/users/register", registerUserController);
-userRouter.get('/users/validate/:registrationCode', validaterUserController);
+userRouter.get("/users/validate/:registrationCode", validaterUserController);
 userRouter.post("/users/login", loginUserController);
 
 userRouter.put("/users/password", editUserPasswordController);
-userRouter.post('/users/password/recover', sendRecoverPassController);
+userRouter.post("/users/password/recover", sendRecoverPassController);
 
 userRouter.post("/newproduct", authUser, adminUser, newProductController);
-userRouter.get('/categories', listCategoriesController);
+userRouter.get("/categories", listCategoriesController);
 
 export default userRouter;
