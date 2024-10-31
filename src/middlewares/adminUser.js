df5f5c30451a5b26e06ec764bb533e6b4e35f9e7
Alex Riera <alrron14@hotmail.com>
2024-10-31 17:08:59 +0100

    Update adminUser.js

--- src/middlewares/adminUser.js
+++ src/middlewares/adminUser.js
@@ -1,22 +1,10 @@
-import getPool from "../database/getPool.js";
 import generateErrorsUtils from "../utils/generateErrorsUtils.js";
 
-const adminUser = async (req, res, next) => {
-  const pool = await getPool();
-  const userId = req.user.id;
-
-  const query = "SELECT role FROM users WHERE id = ?";
-  pool.query(query, [userId], (err, results) => {
-    if (err) {
-      throw generateErrorsUtils("error de servidor", 500);
-    }
-
-    if (results.length === 0 || results[0].role !== "admin") {
-      throw generateErrorsUtils("acceso denegado", 403);
-    }
-
-    next();
-  });
+const adminUser = async (req, next) => {
+  if (req.user.role !== "admin") {
+    throw generateErrorsUtils("acceso denegado", 401);
+  }
+  next();
 };
 
 export default adminUser;
