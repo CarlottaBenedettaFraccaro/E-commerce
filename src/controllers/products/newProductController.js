4ab0e958e4d921e570cedffa52fe76ae63488dc2
Alex Riera <alrron14@hotmail.com>
2024-11-04 10:38:00 +0100

    newProductcontroller changed

--- src/controllers/products/newProductController.js
+++ src/controllers/products/newProductController.js
@@ -1,36 +1,24 @@
-import getPool from "../../database/getPool.js";
+import createProductService from "../../services/products/createProductService.js";
 import generateErrorsUtils from "../../utils/generateErrorsUtils.js";
 
-const newProductController = async (req, res) => {
-  const pool = await getPool();
-  const { product_name, product_description, size, cost } = req.body;
-
+const newProductController = async (req, res, next) => {
   try {
-    await pool.beginTransaction();
+    const { product_name, product_description, size, prize } = req.body;
 
-    const productsQuery = `
-            INSERT INTO products (product_name, product_description)
-            VALUES (?, ?)
-        `;
-    await pool.query(productsQuery, [product_name, product_description]);
-
-    const productSizeQuery = `
-            INSERT INTO product_size (size, cost)
-            VALUES (?, ?)
-        `;
-    await pool.query(productSizeQuery, [size, cost]);
-
-    await pool.commit();
+    await createProductService({
+      product_name,
+      product_description,
+      size,
+      prize,
+    });
 
     res.status(201).json({
       status: "ok",
       data: "Producto y tamaño añadidos a la base de datos",
     });
   } catch (err) {
-    await pool.rollback();
-    console.error("Error al realizar la transacción:", err);
-
-    throw generateErrorsUtils("error al realizar la transacción", 500);
+    console.error("Error en el controlador:", err);
+    next(err);
   }
 };
 
