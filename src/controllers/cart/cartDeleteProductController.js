a29b61bd7038b2fc515509e232fb00566993676a
CarlottaBenedettaFraccaro <benedettaibarra@gmail.com>
2024-11-04 10:25:16 +0100

    new cart controllers
    Controladores eliminar y modificar producto carrito actualizados

--- src/controllers/cart/cartDeleteProductController.js
+++ src/controllers/cart/cartDeleteProductController.js
@@ -1,26 +1,12 @@
 import cartController from cartController.js //aquí hay que importar desde el archivo creado por Alex
 import generateErrorsUtils from generateErrorsUtils.js
+//falta importar el archivo service cuando lo suba 
 
 const removeFromCart = async (req, res, next) => {
     const { userId, productId } = req.params;
 
     try {
-        // Obtener el carrito del usuario
-        const cart = await cartController.findOne({ user: userId });
-        
-        if (!cart) {
-            throw generateErrorsUtils('Carrito no encontrado', 404);
-        }
-
-        // Verificar si el producto estaba en el carrito
-        if (newItems.length === cart.items.length) {
-            throw generateErrorsUtils('Producto no encontrado en el carrito', 404);
-        }
-
-        /* Actualizar el carrito con los nuevos productos
-        cart.items = newItems;
-        await cart.save(); */
-
+        await cartDeleteProductService (userId, productId) 
         res.status(200).json({ message: 'Producto eliminado del carrito', cart });
     } catch (error) {
         next(error);
