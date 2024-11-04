76536b1ebde57fbe8ffd8ec4608f596d1d94c326
Alex Riera <alrron14@hotmail.com>
2024-11-04 10:36:37 +0100

    add product controller changed

--- src/controllers/cart/cartAddProductController.js
+++ src/controllers/cart/cartAddProductController.js
@@ -1,41 +1,20 @@
-const Cart = require("../models/Cart");
+import addProductToCart from "../services/cartService.js";
+import generateErrorsUtils from "../utils/generateErrorsUtils.js";
 
-const cartAddProductController = async (req, res) => {
+const cartAddProductController = async (req, res, next) => {
   try {
-    const { userId, productId, quantity } = req.body;
+    const { userId, productSizeId, quantity } = req.body;
 
-    let cart = await Cart.findOne({ userId });
-
-    if (cart) {
-      // si el carrito ya existe, actualiza la cantidad o añade un nuevo item
-      const itemIndex = cart.items.findIndex(
-        (item) => item.productId == productId
-      );
-
-      if (itemIndex > -1) {
-        // si roducto ya en el carrito, actualiza la cantidad
-        let item = cart.items[itemIndex];
-        item.quantity += quantity;
-        cart.items[itemIndex] = item;
-      } else {
-        // Producto no está en el carrito, añádelo
-        cart.items.push({ productId, quantity });
-      }
-    } else {
-      // Crear un nuevo carrito para el usuario
-      cart = new Cart({
-        userId,
-        items: [{ productId, quantity }],
-      });
-    }
-
-    await cart.save();
-    res.send({
-      status: "ok",
-      message: "añadido al carrito",
+    const response = await addProductToCart({
+      userId,
+      productSizeId,
+      quantity,
     });
+
+    res.status(200).json(response);
   } catch (error) {
-    next(error);
+    console.error("Error en el controlador:", error);
+    next(generateErrorsUtils(error.message, 500));
   }
 };
 
