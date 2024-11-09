e0562f0303b363709207fe3969b905335150081c
mariCarmen <mcmosa1991@gmail.com>
2024-11-09 13:27:44 +0100

    subiendo login y Context

--- ecommerce-front/BackEnd/src/routes/cartRouter.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import listCategoriesController from "../controllers/products/ListCategoriesController.js";
-
-const cartRouter = express.Router();
-
-cartRouter.get("/cart/categories", listCategoriesController);
-
-export default cartRouter;
